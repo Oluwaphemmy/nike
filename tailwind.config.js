ea35537ac1c6b5c7c6ed26f7ba6011c1f62641a0
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-06 10:42:32 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        grad: "linear-gradient(110.8deg, #14A4EE -7.36%, #126BA8 9.67%, #0C3A6B 47.81%, #081F49 76.65%)",
+      },
+      fontFamily: {
+        pop: ["'Poppins', sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
